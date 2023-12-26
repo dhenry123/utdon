0bfcf70f97e1d7e1af2293a6e663e4e66ec8296d
dhenry123 <dhenrystrikteam@gmail.com>
2023-12-26 19:10:59 +0100

    docker & legacy

---
+++ install-legacy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+installdir="/usr/local/utdon"
+service="utdon"
+
+# NodeJS is needed
+which node >/dev/null 2>&1
+if [ "$?" == "1" ]; then
+    echo "You have to install nodejs package (==LTS-20)"
+    exit 1
+fi
+
+# Build application
+npm install && npm run build
+cd client && npm install && npm run build && cd ..
+rm -r node_modules && npm install --omit=dev
+
+sudo mkdir -p $installdir/public $installdir/data
+sudo cp -R dist/* $installdir/.
+sudo cp -R client/dist/* $installdir/public/.
+sudo cp -R node_modules $installdir/.
+rm -r node_modules dist client/node_modules client/dist
+
+# Service installation
+echo "################"
+echo ""
+echo "UTDON has been built and installed in the directory '$installdir'"
+echo ""
+echo "- Create user/group: $service/$service"
+echo ""
+echo "- Then 'chown $service:$service $installdir/data"
+echo ""
+echo "- Generate two secrets"
+USER_ENCRYPT_SECRET="$(openssl rand -base64 32)"
+echo "USER_ENCRYPT_SECRET=$USER_ENCRYPT_SECRET"
+DATABASE_ENCRYPT_SECRET="$(openssl rand -base64 32)"
+echo "DATABASE_ENCRYPT_SECRET=$DATABASE_ENCRYPT_SECRET"
+echo ""
+echo "*** Keep both secrets safe.... ***"
+echo ""
+echo "- Create service in /etc/systemd/system/$service.service"
+echo ""
+cat <<EOF
+[Unit]
+Description=UTDON - Application for tracking obsolete FOSS applications
+After=networking.service
+[environment]
+USER_ENCRYPT_SECRET=$USER_ENCRYPT_SECRET
+DATABASE_ENCRYPT_SECRET=$DATABASE_ENCRYPT_SECRET
+[Service]
+WorkingDirectory=$installdir
+ExecStart=node main.js
+User=$service
+Group=$service
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=$service
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+echo ""
+echo ""
+echo "Start service: 'systemctl start $service'"
+echo ""
+echo "################"
