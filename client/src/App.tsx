4b20ed3247bb6fc298bed42bece64cff826c06db
DHENRY - Mytinydc.com <dhenrystriketeam@gmail.com>
2023-12-31 11:56:37 +0100

    Language (#7)
    * swagger impl core
    
    * screenshot
    
    * language auto detect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,14 +6,25 @@
 // icons
 import "@tabler/icons-webfont/tabler-icons.min.css";
 
-import { useAppSelector } from "./app/hook";
+import { useAppDispatch, useAppSelector } from "./app/hook";
 import { IntlProvider } from "react-intl";
 import ServiceMessage from "./components/ServiceMessage";
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./app/Router";
+import { useEffect } from "react";
+import { setLanguage } from "./app/contextSlice";
 
 export const App = () => {
+  const dispatch = useAppDispatch();
   const contextLanguage = useAppSelector((state) => state.context.language);
+
+  useEffect(() => {
+    // Browser language detection
+    const navigatorLocale =
+      navigator.language.split("-")[0].toLowerCase() !== "fr" ? "en" : "fr";
+    console.log("navigator", navigatorLocale);
+    dispatch(setLanguage(navigatorLocale));
+  }, []);
 
   return (
     <div className="main">
