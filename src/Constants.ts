455f8d681af14d563052c8b1fec47a64c6747644
dHENRY <dhenry@mytinydc.com>
2024-09-17 06:43:40 +0200

    Gitea repo integration
    - [X] update version

--- src/Constants.ts
+++ src/Constants.ts
@@ -25,7 +25,7 @@ export const INPROGRESS_UPTODATEORNOTSTATE: UptoDateOrNotState = {
   urlProduction: "",
 };
 
-export const APPLICATION_VERSION = "1.6.0";
+export const APPLICATION_VERSION = "1.7.0";
 
 // routes which dont need authentication to be served
 // firsts are UI routes
