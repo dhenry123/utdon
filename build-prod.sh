8a2da7ba1ac1cf5f2a5f503d3e7c303e32125482
dHENRY <dhenry@mytinydc.com>
2024-10-02 18:56:26 +0200

    builder

--- build-prod.sh
+++ build-prod.sh
@@ -2,11 +2,14 @@
 # @author DHENRY for mytinydc.com
 # @license AGPL3
 
+set -e
+
 source .envlocaldev
 # login to github
 echo $CR_PAT | sudo docker login ghcr.io -u $USERNAME --password-stdin
 
 # Prepare buildx multiarch
+sudo docker buildx rm multiarch
 sudo docker buildx create --name multiarch --use
 
 # jq is needed
@@ -19,4 +22,4 @@ NOCACHE="--no-cache"
 PLATFORM="--platform=linux/arm64,linux/amd64"
 TAG=$(jq '.version' package.json | sed -E 's/^"|"$//g')
 PROGRESS="--progress plain"
-sudo docker buildx build --load $PROGRESS $NOCACHE $PLATFORM -t ghcr.io/$USERNAME/utdon:$TAG -f Dockerfile .
+sudo docker buildx build --push $PROGRESS $NOCACHE $PLATFORM -t ghcr.io/$USERNAME/utdon:$TAG -f Dockerfile .
